c7e9692ca62cae1822d424d4d2969c88c9fea457
kerin larico <145733758+kiwigenio@users.noreply.github.com>
2025-08-01 02:59:01 -0500

    actualizacion de las estructuras

--- estructuras.h
+++ estructuras.h
@@ -1,7 +1,8 @@
-// estructuras.hpp
+
 #pragma once
 
-const int TAM_MAX = 10000; // puedes ajustarlo seg�n el tama�o de tu grafo
+const int TAM_MAX = 10000;
+const int PQ_MAX = 10000;
 
 class ColaInt {
 private:
@@ -35,6 +36,54 @@ public:
             frente = (frente + 1) % TAM_MAX;
             return val;
         }
-        return -1; // error
+        return -1;
+    }
+};
+
+
+struct NodoPrioridad {
+    int id;
+    float prioridad;
+};
+
+class ColaPrioridad {
+private:
+    NodoPrioridad datos[PQ_MAX];
+    int cantidad;
+
+public:
+    ColaPrioridad() {
+        cantidad = 0;
+    }
+
+    bool vacia() const {
+        return cantidad == 0;
+    }
+
+    void insertar(int id, float prioridad) {
+        int i = cantidad++;
+        while (i > 0 && prioridad < datos[(i - 1) / 2].prioridad) {
+            datos[i] = datos[(i - 1) / 2];
+            i = (i - 1) / 2;
+        }
+        datos[i] = { id, prioridad };
+    }
+
+    int extraer_min() {
+        if (vacia()) return -1;
+        int id = datos[0].id;
+        NodoPrioridad ultimo = datos[--cantidad];
+
+        int i = 0, hijo;
+        while ((hijo = 2 * i + 1) < cantidad) {
+            if (hijo + 1 < cantidad && datos[hijo + 1].prioridad < datos[hijo].prioridad)
+                hijo++;
+            if (ultimo.prioridad <= datos[hijo].prioridad)
+                break;
+            datos[i] = datos[hijo];
+            i = hijo;
+        }
+        datos[i] = ultimo;
+        return id;
     }
 };
